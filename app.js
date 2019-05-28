bded1768ce7c179b3e7ffee718fcad050846bdf2
Halil Taşkit <44812001+haliltaskit@users.noreply.github.com>
2019-05-28 06:13:16 +0300

    prompt yerine modal yerleştirildi - mbootstrap var

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-let name = prompt("İsminiz nedir?","Yarışmacı");
-let user = document.getElementById("user-label");
-user.innerHTML=name;
-// defaultGiris.classList.add('user');
-// defaultGiris.append(" : Welcome " + name);
+const name_input = document.querySelector('#nameForm');
+const user_div = document.getElementById("user-label");
+let user = user_div;
+
+$(window).on('load', function () {
+    $('#modalLoginAvatar').modal('show');
+});
+
+$('#btnSend').on('click', function () {
+});
+
+$('#btnSend').on('keydown click', function (event) {
+    user.innerHTML = $(name_input).val();
+    if (event.keyCode === 13 || event.type === 'click') {
+        $('#modalLoginAvatar').modal('hide');
+        console.log(user.innerHTML);
+    }
+});
+
+
+// $("#iconHelp").on({
+//     "mouseover": function () {
+//         $(this).tooltip({ items: "#iconHelp", content: "Yardım için tıklayınız" });
+//         $(this).tooltip("enable");
+//     },
+//     "mouseout": function () {
+//         $(this).tooltip("disable");
+//     }
+// });
 let userScore = 0;
 let computerScore = 0;
 // First catch the dump - HTML variables that store dump elements  ! Span tag içerisinde tanımlı olduğu için bu const span ile isimlendirildi.
@@ -12,12 +36,13 @@ const computerScore_span = document.getElementById("computer-score");
 // Class'ı yakalarken querySelector kullandık - tag ile bakmamamak için.
 const scoreBoard_div = document.querySelector(".score-board");
 const result_p = document.querySelector(".result>p");
-const actionMessage_p=document.getElementById("action-message");
+const actionMessage_p = document.getElementById("action-message");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
-const smallUserWord = name.fontsize(3).sup().fontcolor('red');// user tanımlayınca burasını güncelle.
+const smallUserWord = user.innerHTML.fontsize(3).sup().fontcolor('red');// user tanımlayınca burasını güncelle.
 const smallCompWord = "bilgisayar".fontsize(3).sup().fontcolor('orange');
+
 
 main();
 
@@ -31,11 +56,6 @@ function getRandomChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
-
-// function checkName(username)
-// {
-//     return typeof(username)==="string" ? username : "Yarışmacı" ;
-// }
 
 function convertToWord(letter) {
     if (letter === "r") return "Taş";
@@ -71,7 +91,7 @@ function win(userChoice, computerChoice) {
     // Es6 ile $ kullanımı geldi! 
     const userChoice_div = document.getElementById(userChoice);
     result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} ➡️ ${convertToWord(computerChoice)}${smallCompWord}`;
-    actionMessage_p.innerHTML="Kazandın!!!🌊";
+    actionMessage_p.innerHTML = "Kazandın!!!🌊";
     // .classList DOM'la gelen ve bize o elemente ait bütün classları dizi olarak dönen metot. Add ile yeni metot ekledik. Green-glow class'ını verdik.
     userChoice_div.classList.add('green-glow');
     setTimeout(function () { userChoice_div.classList.remove('green-glow') }, 600);
@@ -82,17 +102,17 @@ function lose(userChoice, computerChoice) {
     const compChoice_div = document.getElementById(computerChoice);
     update();
     result_p.innerHTML = `${convertToWord(computerChoice)} ${smallCompWord} ⬅️ ${convertToWord(userChoice)} ${smallUserWord}`;
-    actionMessage_p.innerHTML="Kaybettin !!!🤖";
+    actionMessage_p.innerHTML = "Kaybettin !!!🤖";
     compChoice_div.classList.add('red-glow');
     setTimeout(function () { compChoice_div.classList.remove('red-glow') }, 600);
-    
+
 };
 function draw(userChoice, computerChoice) {
     // userScore++;
     // computerScore++;
     const userChoice_div = document.getElementById(userChoice);
     result_p.innerHTML = `${convertToWord(computerChoice)} ${smallCompWord} 🔃 ${convertToWord(userChoice)} ${smallUserWord}`;
-    actionMessage_p.innerHTML="Berabere !!!🤝";
+    actionMessage_p.innerHTML = "Berabere !!!🤝";
 
     userChoice_div.classList.add('gray-glow');
     setTimeout(function () { userChoice_div.classList.remove('gray-glow') }, 600);
